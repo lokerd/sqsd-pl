95566447b7f0ca9d8ba29a79f73d7b19faead6c4
lokerd@ <lokerd@amazon.com>
2014-10-25 14:40:16 +0000

    maybe

--- sqs.pl
+++ sqs.pl
@@ -3,6 +3,7 @@ use AWS::SQSD;
 use strict;
 use warnings;
 use lib qw(.);
+use Data::Dumper;
 
 # Fast pipes
 $| = 1;
@@ -15,9 +16,11 @@ while (1)
 {
 	# Try pop a message off the queue
 	my $res = $sqsd->get_message();
+	print Dumper $res;
 	# Process it if we find one
         if(@{$res->{Messages}}[0]) {
                 $sqsd->process(@{$res->{Messages}}[0]->{Body});
+		$sqsd->rm(@{$res->{Messages}}[0]->{ReceiptHandle});
         }
 	# Time till we try again
 	sleep 20;
