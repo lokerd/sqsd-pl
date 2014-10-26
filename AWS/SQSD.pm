9b3b1c3786fb1a1e050bc43c543525a62da43986
Darren Loker <d.dev.bb@gmail.com>
2014-10-26 22:05:49 +1100

    closing

--- AWS/SQSD.pm
+++ AWS/SQSD.pm
@@ -1,18 +1,19 @@
 package AWS::SQSD;
 
+use strict;
+use warnings;
+
 use POSIX qw(setsid);
 use AWS::CLIWrapper;
 use LWP::UserAgent;
 use LWP::Protocol::https;
 use HTTP::Request::Common;
 use JSON;
-use strict;
-use warnings;
 
 use Data::Dumper;
-
 # Functions for the SQS Daemon
 # lokerd@amazon.com
+
 
 # AWS::SQSD->new()		: constructor
 # AWS::SQSD->get_message()	: retrieve from queue
@@ -21,20 +22,12 @@ use Data::Dumper;
 
 # Regex placeholders
 use constant {
-	REGION      => '___region___',
-	Q 	        => '___queue___',
-	ALLOC		=> '___alloc___',
-	TARGET		=> '___target___'
+	REGION  => '___region___',
+	Q       => '___queue___',
+	QURL    => '___qurl___',
+	ALLOC   => '___alloc___',
+	TARGET  => '___target___'
 };
-
-# Debug		: debug(string)
-# ARGS		: $debug_string - string to be printed
-# RETURNS	: NONE
-# OBJECT	: NONE	
-sub debug {
-	my $debug_string = shift;
-	printf("%.3f :: %-12s\n", time, $debug_string);
-}
 
 # Constructor 	: new({})
 # ARGS		: anonymous hash of constructor args - placeholder
@@ -44,156 +37,162 @@ sub new
 	my ($class, $args) = @_;
 	my $self = {
 		queue => +Q,
+                queue_url => +QURL,
 		region => +REGION,
 		eip_status => 'false',
 		eip_assoc => "",
 		args => $args
 	};
 	bless ($self, $class);
-	$self->_init;
+	$self->_init();
 	return $self; 
 }
 
-# Initialise 	: _init()
-# ARGS:		: NONE
-# RETURN:	: NONE
-# SETS		: $self->{_aws} - AWS::CLIWrapper object
-# 		: $self->{_ua} - LWP object
-# 		: $self->{_q_url} - full URL of SQS Queue
-# 		  returned from &api_call(sqs,create-queue...') 
+# _init()
+# ARGS		: NONE
+# RETURN	: $self->{_aws}         - AWS::CLIWrapper object
+# 		: $self->{_ua}          - LWP::UserAgent object
 sub _init 
 {
 	my $self = shift;
-	my $res;
 	eval 
 	{
 		$self->{_aws} = AWS::CLIWrapper->new(region => $self->{region});
 		$self->{_ua} = LWP::UserAgent->new();
-		$res = $self->api_call('sqs', 'create-queue', {
-				'queue-name' => $self->{queue}
-		});
 	}; 
-	if ($res) { $self->{'_q_url'} = $res->{QueueUrl} }
-	else { die $@ } 
+	die $@ if $@; 
 	$self->daemon();
 	return $self;
 }   
 
-# MSG Worker	: get_message()
+# get_message()
 # ARGS 		: NONE
-# RETURN	: anonymous hash $message
-# 		: undef if no $message
+# RETURN	: anonymous hash $message or undef
 sub get_message 
 {
 	my $self = shift;
 	my $message = $self->api_call('sqs', 'receive-message', {
-			'queue-url' => $self->{_q_url}
+			'queue-url' => $self->{queue_url}
 	});
 	if ( $message ) { return $message }
 	else { return undef }
 }
 
+# rm(str)
+# ARGS 		: $handle - sqs message receipt handle
+# RETURN	: true if successful, undef otherwise
 sub rm 
 {
 	my ($self,$handle) = @_;
 	my $res = $self->api_call('sqs', 'delete-message', {
-			'queue-url' => $self->{_q_url},
+			'queue-url' => $self->{queue_url},
 			'receipt-handle' => $handle 
 	});
 	if ( $res ) { return $res }
 	else { return undef }
 }
 
+sub disassociate 
+{
+	my ($self, $response) = @_;
+	my $res = $self->api_call('ec2', 'disassociate-address', {
+		'instance-id' => +TARGET,
+		'association-id' => $self->{eip_assoc}
+	});
+        my $return = undef;
+	if ( $res->{return} eq 'true' ) 
+	{
+		$self->{eip_status} = 'false';
+		$self->{eip_assoc} = "";
+		$return = $self->s3_signal($response, 'SUCCESS', undef);
+	} 
+	else { $return = $self->s3_signal($response, 'FAILURE', "disassociate-address failed") }
+	if ( $return ) { return $return }
+	else { return undef }
+}
 
-# MSG Processor	: process({})
-# ARGS		: $message - anonymous message hash
-# 		  returned by AWS::SQSD->get_message()  
-# RETURN	: NONE
-sub process 
+sub associate 
+{
+	my ($self, $response) = @_;
+	my $res = $self->api_call('ec2', 'associate-address', {
+		'instance-id' => +TARGET,
+		'allocation-id' => +ALLOC
+	});
+        my $return = undef;
+	if ( $res->{AssociationId} ) 
+	{
+		$self->{eip_status} = 'true';
+		$self->{eip_assoc} = $res->{AssociationId};
+		$return = $self->s3_signal($response, 'SUCCESS', undef);
+	} 
+	else { $return = $self->s3_signal($response, 'FAILURE', "associate-address failed") }
+	if ( $return ) { return $return }
+	else { return undef }
+}
+
+sub message_decode 
 {
 	my ($self, $message) = @_;
     	my $tmp = decode_json $message;
 	my $body = decode_json $tmp->{Message};
-
+        $self->{response_url} = $body->{ResponseURL};
 	my $response = {
 		'RequestId' => $body->{RequestId},
 		'StackId' => $body->{StackId},
 		'LogicalResourceId' => $body->{LogicalResourceId},
 		'PhysicalResourceId' => 'lokerd'
 	};
+	if($response and $body) 
+        { return($response, $body) }
+	else { return undef }
+}
 
+# process({})
+# ARGS		: $message - anonymous message hash
+# 		  returned by AWS::SQSD->get_message()  
+# RETURN	: true if successful or undef
+sub process 
+{
+	my ($self, $message) = @_;
+        my ($response, $body) = $self->message_decode($message);
+        my $return = undef;
 	if ( $body->{ResourceProperties}{Attach} eq $self->{eip_status} ) 
-	{ $self->success($response,$body->{ResponseURL}) } 
+	{ $return = $self->s3_signal($response, 'SUCCESS', undef) } 
 	else
 	{
 		if ( $self->{eip_status} eq 'false' ) 
-		{ 
-			my $message = $self->api_call('ec2', 'associate-address', {
-				'instance-id' => +TARGET,
-				'allocation-id' => +ALLOC
-			});
-			if ( $message->{AssociationId} ) 
-			{
-				$self->{eip_status} = 'true';
-				$self->{eip_assoc} = $message->{AssociationId};
-				$self->success($response,$body->{ResponseURL});
-			} 
-			else { $self->fail($response,$body->{ResponseURL}) }
-		} 
-		else 
-		{
-			my $message = $self->api_call('ec2', 'disassociate-address', {
-				'instance-id' => +TARGET,
-				'assocation-id' => $self->{eip_assoc}
-			});
-			if ( $message->{return} eq 'true' ) 
-			{
-				$self->{eip_status} = 'false';
-				$self->{eip_assoc} = "";
-				$self->success($response, $body->{ResponseURL});
-			} 
-			else { $self->fail($response, $body->{ResponseURL}) }
-		}
+		{ $return = $self->associate($response) } 
+		else { $return = $self->disassociate($response) }
 	}	
-
-	return;
+	if ($return) { return $return}
+	else { return undef }
 }
 
-sub success 
-{
-	my ($self, $response, $response_url) = @_;
-	$response->{Status} = 'SUCCESS';
-	$response->{Data}{AttachmentStatus} = $self->{eip_status};
-	$self->s3_signal($response,$response_url);
-}
-	
-sub fail 
-{
-	my ($self, $response, $response_url) = @_;
-	$response->{Status} = 'FAILED';
-	$response->{Data}{AttachmentStatus} = $self->{eip_status};
-	$self->s3_signal($response,$response_url);
-}
-
+# s3_signal({} str)
+# ARGS 		: $response - CF response anon hash
+#               : $response_url - from CF notification
+# RETURN	: true if rc 200, undef otherwise
 sub s3_signal
 {
-	my ($self, $response, $response_url) = @_;
+	my ($self, $response, $status, $reason) = @_;
+        if ($status eq 'FAILED') { $self->{Reason} = $reason };  
+	$response->{Status} = $status;
+	$response->{Data}{AttachmentStatus} = $self->{eip_status};
 	my $json = encode_json($response);
-	my $req = PUT $response_url;
+	my $req = PUT $self->{response_url};
 	$req->header( 'Content-Type' => '' );
 	$req->content( $json );
 	my $res = $self->{_ua}->request($req);
-	if ($res->{_rc} eq '200') { print Dumper $res and return }
-	else { print Dumper $res and return undef }
+	if ($res->{_rc} eq '200') { return }
+	else { return undef }
 }
 
-# API Worker	: api_call(str,str,{})
-# ARGS:		: $service - aws cli service eg. [sqs,autoscaling,sns...]
+# api_call(str str {})
+# ARGS		: $service - aws cli service eg. [sqs,autoscaling,sns...]
 # 		: $operation - cli operation eg. [delete-message,create-queue,...]
 # 		: $params - anonymous hash of option/argument pairs
 # 		  $params = { 'option' => '$argument', 'queue-url' => '$q' }
-# RETURN:	: $res - anonymous hash of the response
-# 		: undef if no $res
+# RETURN	: $res - anonymous hash of the response or undef
 sub api_call {
 	my ($self, $service, $operation, $params) = @_;
 	my $res = $self->{_aws}->$service($operation, $params);
@@ -206,21 +205,27 @@ sub api_call {
 	}
 }
 
-# Daemonize	: daemon()
+# daemon()
 # ARGS		: NONE
 # RETURN	: NONE
-# OBJECT	: NONE
-# STDIN 	: DETACHED
 # STDOUT, STDERR: /var/log/sqsd{-error}.log
 sub daemon {
 	chdir '/' or die "Can't chdir to /: $!";
 	open STDIN, '/dev/null' or die "Can't read /dev/null: $!";
-	open STDOUT, '>>/var/log/sqsd.log' or die "Can't write to /dev/null: $!";
-	open STDERR, '>>/var/log/sqsd-error.log' or die "Can't write to /dev/null: $!";
+	open STDOUT, '>>/var/log/sqsd.log' or die "Can't write to /var/log/sqsd.log: $!";
+	open STDERR, '>>/var/log/sqsd-error.log' or die "Can't write to /var/log/sqsd-error.log: $!";
 	defined(my $pid = fork) or die "Can't fork: $!";
 	exit if $pid;
 	setsid or die "Can't start a new session: $!";
 	umask 0;
+}
+
+# Debug		: debug(string)
+# ARGS		: $debug_string - string to be printed
+# RETURNS	: NONE
+sub debug {
+	my $debug_string = shift;
+	printf("%.3f :: %-12s\n", time, $debug_string);
 }
 
 1;
